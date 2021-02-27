a008932949382ab887e548fb6ec278d94194b146
paradise <lin92731@outlook.com>
2021-02-27 21:09:58 +0800

    chore: remove some comments

--- src/dataStructures/LinkedList/leetcode/leetcode-203.ts
+++ src/dataStructures/LinkedList/leetcode/leetcode-203.ts
@@ -3,8 +3,6 @@
 import ListNode from './ListNode'
 
 // 最早在leetcode中提交的是这一份代码，相比递归来说略显麻烦
-// 执行用时：100 ms, 在所有 TypeScript 提交中击败了94.00% 的用户
-// 内存消耗：43.6 MB, 在所有 TypeScript 提交中击败了98.00% 的用户
 function removeElements(head: ListNode | null, val: number): ListNode | null {
   while (head != null && head.val === val) {
     const delNode = head
@@ -33,8 +31,6 @@ function removeElements(head: ListNode | null, val: number): ListNode | null {
 
 // 使用虚拟头节点解决head需要特殊处理的情况
 // 减少代码复杂度
-// 执行用时： 108 ms , 在所有 TypeScript 提交中击败了 64.00% 的用户
-// 内存消耗： 43.6 MB , 在所有 TypeScript 提交中击败了 98.00% 的用户
 function removeElements2(head: ListNode | null, val: number): ListNode | null {
   const dummyHead = new ListNode()
   dummyHead.next = head
@@ -53,8 +49,6 @@ function removeElements2(head: ListNode | null, val: number): ListNode | null {
 }
 
 // 使用递归
-// 执行用时： 112 ms , 在所有 TypeScript 提交中击败了 38.00% 的用户
-// 内存消耗： 44.1 MB , 在所有 TypeScript 提交中击败了 98.00% 的用户
 function removeElements3(head: ListNode | null, val: number): ListNode | null {
   if (head == null) {
     return null
